5937aadb62142dbe100a03068112c63ffce3d116
Chuhtra <steliosKs@protonmail.com>
2020-12-11 23:02:05 +0200

    Added the method that loads the dataset.

--- ex2.2.py
+++ ex2.2.py
@@ -1,5 +1,25 @@
+import numpy as np
+import pandas as pd
+
+
+def loaddataset():
+    datafile = "../UCIdata-exercise1/pima-indians-diabetes.data"
+    namesfile = "../UCIdata-exercise1/pima-indians-diabetes.names"
+
+    names = open(namesfile).readlines()
+    for i in names: print(i)
+
+    data = np.genfromtxt(datafile, delimiter=",")
+    features = data[:, :-1]
+    category = data[:, -1:]
+
+    return features, category
+
+
 def main():
     print("hello")
+    features, category = loaddataset()
+
 
 if __name__ == "__main__":
     main()
